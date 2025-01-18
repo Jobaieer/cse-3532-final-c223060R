f6efa2cf19b9a0ff3068195b298f01384dfe6cf8
Jobaieer <jobaieer.cse@proton.me>
2025-01-19 03:19:18 +0600

    added Blog.jsx in route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Blog from "./pages/Blog";
 import All from "./pages/All";
 import Music from "./pages/Music";
 import Comedy from "./pages/Comedy";
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/music" element={<Music />} />
         <Route path="/comedy" element={<Comedy />} />
         <Route path="/drawing" element={<Drawing />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </Router>
   );
